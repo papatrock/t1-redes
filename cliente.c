f8848716634bdbdf984e91bd42fc2458b662c421
papatrock <ptckxx@gmail.com>
2024-10-30 17:50:08 -0300

    enviando msg, falta testar no dinf

--- cliente.c
+++ cliente.c
@@ -102,17 +102,21 @@ int criasocket(char *interface)
 int main(){
 	//struct sockaddr_ll endereco;
     int soquete = criasocket(INTERFACE); 
-   
+    
+    int ifindex = if_nametoindex(INTERFACE);
+    struct sockaddr_ll endereco = {0};
+    endereco.sll_family = AF_PACKET;
+    endereco.sll_protocol = htons(ETH_P_ALL);
+    endereco.sll_ifindex = ifindex;
 
 	const char *mensagem = "SAAAAAAAAAAAAAAAAAAAAAAAALVE";
-	/*if(sendto(soquete,mensagem,strlen(mensagem),0,(struct sockaddr*)&endereco, sizeof(endereco)) ==-1)
-
+	if(sendto(soquete,mensagem,strlen(mensagem),0,(struct sockaddr*)&endereco, sizeof(endereco)) ==-1)
 	{
-		perror("erro ao enviar mensagem");
+		printf("erro ao enviar mensagem\n");
 	}
 	else
 		printf("Mensagem enviada com sucesso\n");
-*/
+
 	close(soquete);
     return 0;
 
