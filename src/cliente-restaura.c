1b3c624a0ffea2aa63ca582716b2bb2857556cc6
Erick.Graeff <erickgraeff@gmail.com>
2024-11-24 23:19:47 -0300

    validando se arquivo ja existe e montando versoes dele

--- src/cliente-restaura.c
+++ src/cliente-restaura.c
@@ -22,46 +22,85 @@ void handle_restaura(char* nome_arq, struct sockaddr_ll endereco, int soquete, u
     #endif
 
     //TODO tratar resposta
+    unsigned char* data = getDados((*bufferResposta));
+    long int file_size = -1;
     switch (getTipo((*bufferResposta)))
     {
     case OK_TAM:
-        unsigned char* tamanho = getDados((*bufferResposta));
-        printf("Tamanho do arquivo: %s\n", tamanho);
+        file_size = atol((char*) data);
+        printf("Tamanho do arquivo: %ld\n", file_size);
+
         break;
     case ERRO:
-        char* erro = getErrors((*bufferResposta));
-        printf("ERRO: %s\n", erro);
-        break;
-    
+        printf("ERRO: %s\n", getErrors(data));
+        return;
+    case NACK:
+        printf("NACK\n"); // ADICIONAR TRATAMENTO
+        return;
     default:
         break;
     }
 
-    // char path[100]; 
-    // strcpy(path, "Cliente/"); 
-    // strcat(path, nome_arq);
-    // FILE *arq;
-    // if(access(path, F_OK) == 0) {
-    //     printf("Já existe um arquivo com esse nome. O que deseja fazer? (Digite o número que está ao lado da opção desejada)\n");
-    //     printf("1. Sobrescrever\n");
-    //     printf("2. Criar um novo arquivo\n");
-    //     printf("3. Cancelar\n");
+    // possivelmente esse tratamento aqui nao faz sentido
+    if(file_size == -1) {
+        printf("Erro ao obter tamanho do arquivo\n");
+        return;
+    }
 
-    //     char restaura_action;
-    //     scanf("%c", &restaura_action);
-    //     switch (restaura_action)
-    //     {
-    //     case '1':
-    //         arq = fopen(path, "w");
-    //         break;
-    //     case '2':
-    //         int i = 1;
-    //         char path_new_file[100];
-    //         strcpy(path_new_file, path);
+    // identifica se arquivo ja existe. Se existir, pergunta ao usuario a acao que ele quer fazer
+    char path[100]; 
+    strcpy(path, "Cliente/"); 
+    strcat(path, nome_arq);
+    FILE *arq;
+    if(access(path, F_OK) == 0) {
+        char restaura_action;
+        do
+        {
+            printf("Já existe um arquivo com esse nome. O que deseja fazer? (Digite o número que está ao lado da opção desejada)\n");
+            printf("1. Sobrescrever\n");
+            printf("2. Criar um novo arquivo (enumerado, por exemplo <nome_arquivo_x> onde x eh a versao ainda nao existente)\n");
+            printf("3. Cancelar\n");
 
-    //     default:
-    //         break;
-    //     }
-    // }
+            scanf("%c", &restaura_action);
+            printf("oi\n");
+            printf("restaura_action: %c\n", restaura_action);
+            switch (restaura_action)
+            {
+            case '1':
+                arq = fopen(path, "w");
+                break;
+            case '2':
+                int i = 1;
+                char path_new_file[100];
+                char file_version[2];
+                // monta o nome do arquivo com a versao dele no final
+                do {
+                    strcpy(path_new_file, path);
+                    strcat(path_new_file, "_");
+
+                    sprintf(file_version, "%d", i);
+                    strcat(path_new_file, file_version);
+                    i++;
+                } while(access(path_new_file, F_OK) == 0 && i < 100);
+                if(i == 100) {
+                    printf("Numero maximo de versoes atingido\n");
+                    // TODO mandar mensagem de erro (criar novo tipo de erro)
+                    return;
+                }
+                arq = fopen(path_new_file, "w");
+                break;
+            case '3':
+                printf("Operação cancelada\n");
+                // TODO mandar mensagem de erro (criar novo tipo de erro)
+                return;
+            default:
+                printf("Opção inválida\n");
+                break;
+            }
+        } while(restaura_action != '1' && restaura_action != '2' && restaura_action != '3');
+    }
+    else
+        arq = fopen(path, "w");
+
 
 }
